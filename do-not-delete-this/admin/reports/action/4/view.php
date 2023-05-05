ce973539065c4218a77d7bcd03b97f8634224d0d
Adrianne77 <madrioadrean773@gmail.com>
2023-05-05 17:05:09 +0800

    report updates3

--- do-not-delete-this/admin/reports/action/4/view.php
+++ do-not-delete-this/admin/reports/action/4/view.php
@@ -61,7 +61,7 @@ $personnel = $pdo->query("SELECT * FROM report_personnel_list")->fetchAll();
                                 <td><?php echo $row['date'] ?></td>
                                 <td>
                                     <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-                                        VIEW</button>
+                                        <a href="../../pdf/pam_pdf.php?view_id=<?php echo $row['pam_id']; ?> " target="_blank">VIEW</button>
                                     <button onclick="return confirm('are you sure?')" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                                         <a href="delete.php?delete_id=<?php echo $row['pam_id']; ?>"> DELETE</a></button>
                                 </td>
--- do-not-delete-this/admin/reports/pdf/accomplishment_pdf.php
+++ do-not-delete-this/admin/reports/pdf/accomplishment_pdf.php
@@ -74,18 +74,4 @@ $pdf->Cell(10, 2, "", 0, 1, 'FJ');
 $pdf->Justify($nr, 190, 6);
 
 
-
-// Instanciation of inherited class
-
-
-
-
-// $pdf = new PDF();
-// $pdf->AddPage();
-// $pdf->SetFont('Arial', '', 9);
-// $pdf->Cell(85, 4, "EXAMPLE OF FUNCTION USAGE", 1, 1, 'C');
-// $pdf->Write(4, "\nSource: http://www.swg-fr.com\n\n");
-// $text = file_get_contents('ex.txt');
-
-// $pdf->Output();
 $pdf->Output();
