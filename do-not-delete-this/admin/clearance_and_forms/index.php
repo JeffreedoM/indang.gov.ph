ff1cb1bdbc11fa866d3785ab79e9d6d12a008d19
Jeffrey Nunez <jeffreynunez029@gmail.com>
2023-06-27 20:36:34 +0800

    offender can't get clearance

--- do-not-delete-this/admin/clearance_and_forms/index.php
+++ do-not-delete-this/admin/clearance_and_forms/index.php
@@ -9,6 +9,13 @@ $stmt->bindParam(':barangay_id', $barangayId, PDO::PARAM_INT);
 $stmt->execute();
 $resident = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+
+// Select all from offenders
+$stmt = $pdo->prepare("SELECT * FROM incident_offender");
+$stmt->execute();
+$incident_offenders = $stmt->fetchAll();
+
+
 $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.resident_id = new_clearance.resident_id;")->fetchAll();
 
 ?>
@@ -29,7 +36,7 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
     <link rel="stylesheet" href="./assets/css/add_finance.css" type="text/css" />
     <link rel="stylesheet" href="./assets/css/popup2.css" type="text/css" />
     <link rel="stylesheet" href="./assets/css/styles2.css" type="text/css" />
-    
+
     <title>Admin Panel</title>
 </head>
 
@@ -53,7 +60,7 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
 
             <!-- Page body -->
             <div class="page-body" style="overflow-x:auto; min-height: 60vh;">
-                
+
 
                 <div class="add_clearance">
                     <button onclick="openInsertPopup()" class="add_transaction"><span>Add Transaction</span></button>
@@ -77,10 +84,10 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
                         </thead>
                         <tbody>
                             <!-- Code to change color based on status -->
-                            <?php foreach($finance as $row) { 
+                            <?php foreach ($finance as $row) {
                                 $color_status = $row['status'];
                                 $color_style = '';
-                                
+
                                 if ($color_status == 'Pending') {
                                     $color_style = 'color: blue;';
                                 } elseif ($color_status == 'Paid') {
@@ -89,30 +96,32 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
                                     $color_style = 'color: red;';
                                 }
                             ?>
-                            <tr>
-                                   
-                                    <td><?php echo $row['form_request']?></td>
-                                    <td><?php echo $row['firstname']; echo ' ' . $row['middlename']; echo ' ' .$row['lastname']?></td>
-                                    <td><?php echo "₱" . $row['amount'];?></td>
-                                    <td><?php echo $row['date_string']?></td>
-                                    <td><span style="<?php echo $color_style ?>"> <?php echo $color_status?></span></td>
+                                <tr>
+
+                                    <td><?php echo $row['form_request'] ?></td>
+                                    <td><?php echo $row['firstname'];
+                                        echo ' ' . $row['middlename'];
+                                        echo ' ' . $row['lastname'] ?></td>
+                                    <td><?php echo "₱" . $row['amount']; ?></td>
+                                    <td><?php echo $row['date_string'] ?></td>
+                                    <td><span style="<?php echo $color_style ?>"> <?php echo $color_status ?></span></td>
                                     <td>
-                                        <button><a href="includes/add_view/add_view.php?finance_id=<?php echo $row['finance_id'] ?>&action=view&resident_id=<?php echo $row['resident_id']?>">View</a></button>
-                                        <button><a href="includes/add_view/add_view.php?finance_id=<?php echo $row['finance_id'] ?>&action=edit&resident_id=<?php echo $row['resident_id']?>">Edit</a></button>
+                                        <button><a href="includes/add_view/add_view.php?finance_id=<?php echo $row['finance_id'] ?>&action=view&resident_id=<?php echo $row['resident_id'] ?>">View</a></button>
+                                        <button><a href="includes/add_view/add_view.php?finance_id=<?php echo $row['finance_id'] ?>&action=edit&resident_id=<?php echo $row['resident_id'] ?>">Edit</a></button>
                                     </td>
-                            </tr>
-                                    <?php } ?>
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
-                
+
             </div>
 
             <!-- Add clearance pop-up -->
-            <div class="add-clearance" id="modal" >
-                
+            <div class="add-clearance" id="modal">
+
                 <div class="content" id="popup">
-                <button class="closebtn"onclick="closePopup()">X</button>
+                    <button class="closebtn" onclick="closePopup()">X</button>
                     <h1 style="margin-bottom: 1rem;">Clearance Name/Type:</h1>
                     <form action="" method="POST" required>
                         <!-- input clearance name/type -->
@@ -120,55 +129,67 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
                             <p></p>
                             <input type="text" name="clearancename" id="clearancename" placeholder="" required>
                         </div>
-                        
-                        <button type="submit" name="submit" id="submitButton" class="submitButton" >Add Clearance</button>
+
+                        <button type="submit" name="submit" id="submitButton" class="submitButton">Add Clearance</button>
                     </form>
                 </div>
             </div>
-            
+
     </main>
-    
+
 
     <!-- Add Officials -->
     <div class="modal-bg2" onclick="closePopup()" id="modal-background">
-        </div>
+    </div>
 
-        <div class="popup2" id="modal-container">
-            <h1>List of Residents</h1>
+    <div class="popup2" id="modal-container">
+        <h1>List of Residents</h1>
 
-            <table id="residents" class="row-border hover">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
+        <table id="residents" class="row-border hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Involved in Incident?</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($resident as $resident) { ?>
+                    <?php
+                    $isOffender = false; // Flag to track if resident is an offender
+                    foreach ($incident_offenders as $offender) {
+                        if ($resident['resident_id'] == $offender['resident_id']) {
+                            $isOffender = true;
+                            break;
+                        }
+                    } ?>
+                    <tr id="<?php echo $resident['resident_id'] ?>" <?php echo $isOffender ? "style='cursor:not-allowed; pointer-events:none;'" : "style='cursor:pointer'" ?>>
+                        <td><?php echo $resident['resident_id'] ?></td>
+                        <td>
+                            <?php
+                            $resident_fullname = "$resident[firstname] $resident[middlename] $resident[lastname]";
+                            echo $resident_fullname;
+                            ?>
+                        </td>
+                        <td><?php echo $isOffender ? "<span style='color:red;'>Yes</span>" : "<span style='color:green;'>No</span>"; ?></td>
                     </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($resident as $resident) { ?>
-                        <tr id="<?php echo $resident['resident_id'] ?>" style="cursor:pointer">
-                            <td><?php echo $resident['resident_id'] ?></td>
-                            <td><?php
-                                $resident_fullname = "$resident[firstname] $resident[middlename] $resident[lastname]";
-                                echo $resident_fullname ?>
-                            </td>
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+                <?php } ?>
+            </tbody>
+        </table>
 
 
-            <!-- close popup button -->
-            <span class="close-popup" onclick="closePopup()">
-                <i class="fa-solid fa-x"></i>
-            </span>
-        </div>
+        <!-- close popup button -->
+        <span class="close-popup" onclick="closePopup()">
+            <i class="fa-solid fa-x"></i>
+        </span>
+    </div>
 
     <!-- insert record modal -->
     <div class="modal2" id="modal_vaccine">
         <div class="header">
             <p class="header-text-vacc"><b>Insert Record</b></p>
             <button class="closebtn2" onclick="closeInsertPopup()">X</button>
-            
+
             <button class="add-resident__button" onclick="openPopup2()">
                 <label for="position" class="block font-medium text-red-500 dark:text-white">Select resident <i class="fa-solid fa-caret-down ml-1"></i></label>
             </button>
@@ -200,8 +221,8 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
                         </select>
                     </div>
                     <div class="wrap-position-sub">
-                       <label for="position" class="block font-medium text-gray-900 dark:text-white">Others</label>
-                        <input type="text" name="form_request_others" placeholder="Others"> 
+                        <label for="position" class="block font-medium text-gray-900 dark:text-white">Others</label>
+                        <input type="text" name="form_request_others" placeholder="Others">
                     </div>
                 </div>
                 <div class="wrap-position">
@@ -222,9 +243,9 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
 
                 <div>
                     <label for="death_cause" class="block font-medium text-gray-900 dark:text-white">Purpose</label>
-                    <textarea name="finance_purpose" id="" cols="67" rows="5" placeholder="Request purpose ..."></textarea>
+                    <textarea name="finance_purpose" id="" rows="5" placeholder="Request purpose ..." class="w-full"></textarea>
                 </div>
-                
+
                 <input type="hidden" name="position_officials" value="">
                 <button onclick="return  confirm('Do you want to add this record?')" type="submit" name="submit_finance" class="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
             </form>
@@ -239,25 +260,26 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <script>
-    $(document).ready( function () {
-    $('#clearance-list').DataTable();
-    } );
+        $(document).ready(function() {
+            $('#clearance-list').DataTable();
+        });
     </script>
     <!-- popup js -->
-        <script>
-            let popup = document.getElementById("popup")
-            let modal = document.getElementById("modal")
-           
-           
-                function openPopup() {
-                    modal.classList.add("modal-active");
-                    popup.classList.add("open-popup");
-                } 
-                function closePopup() {
-                    popup.classList.remove("open-popup");
-                    modal.classList.remove("modal-active");
-                }             
-        </script>
+    <script>
+        let popup = document.getElementById("popup")
+        let modal = document.getElementById("modal")
+
+
+        function openPopup() {
+            modal.classList.add("modal-active");
+            popup.classList.add("open-popup");
+        }
+
+        function closePopup() {
+            popup.classList.remove("open-popup");
+            modal.classList.remove("modal-active");
+        }
+    </script>
 
     <!-- event listener 
         <script>
@@ -285,16 +307,17 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
             }        
         </script> -->
 
-        <script>
+    <script>
         let modal2 = document.getElementById('modal_vaccine');
 
-            function openInsertPopup() {
-                modal2.classList.add("modal-active2");
-            }
-            function closeInsertPopup() {
-                modal2.classList.remove("modal-active2");
-            }
-        </script>
+        function openInsertPopup() {
+            modal2.classList.add("modal-active2");
+        }
+
+        function closeInsertPopup() {
+            modal2.classList.remove("modal-active2");
+        }
+    </script>
 
     <script>
         $(document).ready(function() {
@@ -310,9 +333,9 @@ $finance = $pdo->query("SELECT * FROM resident JOIN new_clearance ON resident.re
         }
     </script>
 
-    
 
-        
+
+
 </body>
 
 </html>
