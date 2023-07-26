ada06cf0a6a2601dd900adebc612c667a9ab8a2a
Jeffrey Nunez <jeffreynunez029@gmail.com>
2023-07-27 00:09:43 +0800

    delete unused session files after 1 hour

--- barangay/includes/session.php
+++ barangay/includes/session.php
@@ -1,4 +1,33 @@
 <?php
 
-session_save_path(dirname(__DIR__) . '/sessions');
+// Set the session save path
+$sessionSavePath = dirname(__DIR__) . '/sessions';
+session_save_path($sessionSavePath);
+
+// Start the session
 session_start();
+
+// Set the idle session time (in seconds)
+$idleSessionTime = 3600; // 1 hour (adjust this as needed)
+
+// Check and delete idle session files
+$currentTime = time();
+$files = glob("$sessionSavePath/sess_*");
+foreach ($files as $file) {
+    if (file_exists($file) && ($currentTime - filemtime($file) > $idleSessionTime)) {
+        unlink($file);
+    }
+}
+
+
+// Set a custom session expiration time (in seconds)
+// $sessionExpiration = 86400; // 24 hours (adjust this as needed)
+
+// // Check and delete old session files
+// $currentTime = time();
+// $files = glob("$sessionSavePath/sess_*");
+// foreach ($files as $file) {
+//     if (file_exists($file) && ($currentTime - filemtime($file) > $sessionExpiration)) {
+//         unlink($file);
+//     }
+// }
