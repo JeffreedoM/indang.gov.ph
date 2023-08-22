6e705b565a0928bd55465ab012843e62b5a05417
Jeffrey Nunez <jeffreynunez029@gmail.com>
2023-08-22 16:15:07 +0800

    update edit form button

--- barangay/admin/clearance_and_forms/edit-form.php
+++ barangay/admin/clearance_and_forms/edit-form.php
@@ -107,7 +107,7 @@ $form = $stmt->fetch();
                     </div>
                     <input type="hidden" name="form_id" value="<?php echo $form_id ?>">
                     <input type="hidden" name="old-form-name" value="<?php echo $form['form_name'] ?>">
-                    <button type="submit" name="update-form" class="tracking-wider w-full mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Create Form</button>
+                    <button type="submit" name="update-form" class="tracking-wider w-full mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit Form</button>
                 </form>
             </div>
         </div>
