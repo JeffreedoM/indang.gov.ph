94534337947be81c6e1a21ffba55b9d3940d5419
Jeffrey Nunez <jeffreynunez029@gmail.com>
2023-08-26 11:56:22 +0800

    added indication of px in ckeditor image properties

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -3,8 +3,22 @@
  * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
  */
 
-CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+  // Define changes to default configuration here. For example:
+  // config.language = 'fr';
+  // config.uiColor = '#AADC6E';
+  CKEDITOR.on("dialogDefinition", function (ev) {
+    // Check if the dialog being defined is the "image" dialog
+    if (ev.data.name === "image") {
+      var dialogDefinition = ev.data.definition;
+
+      // Find the width and height inputs in the dialog
+      var widthInput = dialogDefinition.getContents("info").get("txtWidth");
+      var heightInput = dialogDefinition.getContents("info").get("txtHeight");
+
+      // Change the labels of the width and height inputs
+      widthInput.label = "Width (px)";
+      heightInput.label = "Height (px)";
+    }
+  });
 };
