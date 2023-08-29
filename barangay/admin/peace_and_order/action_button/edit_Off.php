c833380e304f9122cd83aa0cad7537460826f4b8
Jeffrey Nunez <jeffreynunez029@gmail.com>
2023-08-29 15:26:15 +0800

    fix edit offender modal

--- barangay/admin/peace_and_order/action_button/edit_Off.php
+++ barangay/admin/peace_and_order/action_button/edit_Off.php
@@ -18,7 +18,7 @@
             <?php include '../includes/resident_off.php'; ?>
             <div id="o_input">
                 <!--Modal for selectiung offender  -->
-                <div id="o_resident" style="display:none;">
+                <div id="o_resident" style="display:none; width:fit-content;">
                     <button data-modal-target="offenderModal" data-modal-toggle="offenderModal" class="block mb-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
                         Select Resident
                     </button>
